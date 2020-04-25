6ae6f2add381cff3f12a76001f37efaf9130765c
Adnane Elbakkali <adnane@elbakkali.me>
2020-04-25 15:36:05 +0200

    displaying results in a table

--- pwdsnoopy.py
+++ pwdsnoopy.py
@@ -6,11 +6,24 @@ result = subprocess.run(
     'grep -H ^psk= /etc/NetworkManager/system-connections/*', capture_output=True, text=True, shell=True
 ).stdout.strip().split('\n')
 
+ssids = []
+passwords = []
 credentials = []
 
 for val in result:
     val = list(filter(None, val.rsplit('/')))
     val = val[-1].split(':psk=')
+    ssids.append(val[0])
+    passwords.append(val[1])
     credentials.append({'ssid': val[0], 'password': val[1]})
 
-print(credentials)
+longestSsid = len(max(ssids, key=len))
+longestpwd = len(max(passwords, key=len))
+
+tableHeader = "SSID" + (" " * (longestSsid - len('SSUD') + 1)) + "| Password"
+print(tableHeader)
+print('-' * (longestSsid + longestpwd + 5))
+
+for c in credentials:
+    row = c['ssid'] + (' ' * (longestSsid - len(c['ssid']) + 1)) + '| ' + c['password']
+    print(row)
