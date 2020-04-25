d5f53ca61510fdb4d20e46e5d41852f4974943ba
Adnane Elbakkali <adnane@elbakkali.me>
2020-04-25 22:09:03 +0200

    checking os, implementing linux os case, checking if script is executed with as root

--- pwdsnoopy.py
+++ pwdsnoopy.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3.8
 
 import subprocess
-
-result = subprocess.run(
-    'grep -H ^psk= /etc/NetworkManager/system-connections/*', capture_output=True, text=True, shell=True
-).stdout.strip().split('\n')
+import platform
+import os
 
 ssids = []
 passwords = []
 credentials = []
 
-for val in result:
-    val = list(filter(None, val.rsplit('/')))
-    val = val[-1].split(':psk=')
-    ssids.append(val[0])
-    passwords.append(val[1])
-    credentials.append({'ssid': val[0], 'password': val[1]})
+osName = platform.system()
+
+if osName == 'Linux':
+    if os.geteuid() != 0:
+        exit('This script needs root privilages, please re-execute as root.')
+        
+    result = subprocess.run(
+        'grep -H ^psk= /etc/NetworkManager/system-connections/*', capture_output=True, text=True, shell=True
+    ).stdout.strip().split('\n')
+    
+    for val in result:
+        val = list(filter(None, val.rsplit('/')))
+        val = val[-1].split(':psk=')
+        ssids.append(val[0])
+        passwords.append(val[1])
+        credentials.append({'ssid': val[0], 'password': val[1]})
+
+else:
+    exit('This script does not currently support your OS')
 
 longestSsid = len(max(ssids, key=len))
 longestpwd = len(max(passwords, key=len))
 
 tableHeader = "SSID" + (" " * (longestSsid - len('SSUD') + 1)) + "| Password"
+
 print(tableHeader)
 print('-' * (longestSsid + longestpwd + 5))
 
